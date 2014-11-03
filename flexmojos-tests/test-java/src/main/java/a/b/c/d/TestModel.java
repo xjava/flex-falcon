a9bdaccd3458f8b61d5d534b9b000bf4ccaa2eaf
Christofer Dutz <christofer.dutz@codecentric.de>
2014-11-03 18:44:40 +0100

    - Did some more work on the failing test-modules.

---
+++ flexmojos-tests/test-java/src/main/java/a/b/c/d/TestModel.java
@@ -0,0 +1,14 @@
+package a.b.c.d;
+
+/**
+ * Created by christoferdutz on 03.11.14.
+ */
+public class TestModel {
+
+    public String stingProperty;
+
+    public int intProperty;
+
+    public boolean booleanProperty;
+
+}
